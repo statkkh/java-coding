124dd865be737f3fed5668ade422d38d482063f6
kkh <qr21@naver.com>
2024-03-17 10:07:07 +0900

    feat : 프로그래머스 두 개 뽑아서 더하기 풀이

---
+++ Ex0608.java
@@ -0,0 +1,36 @@
+import java.util.*;
+public class Ex0608 {
+    
+    public static void main(String[] args) {
+        
+    }
+
+    class Solution {
+        public int[] solution(int[] numbers) {
+            Set<Integer> set = new HashSet<>();
+            for(int i = 0 ; i < numbers.length-1 ; i++){
+                for(int j = i + 1; j < numbers.length; j++){
+                    set.add(numbers[i] + numbers[j]);
+                }
+            }
+
+            // List<Integer> list = new ArrayList<>(set);
+            Interator<Integer> list = set.iterator();        
+            
+            // Collections.sort(list);
+            
+            int[] result = new int[set.size()];
+
+            // for (int i = 0; i < list.size(); i++) {
+            //     result[i] = list.get(i);
+            // } 
+            
+            while(list.hasNext()){
+                
+            }
+            
+            return result;
+        }
+    }  
+}
+     
