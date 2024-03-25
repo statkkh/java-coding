5dd3c5cacdae89f073426d7270c06cb56dbc6dec
kkh <qr21@naver.com>
2024-03-25 10:29:02 +0900

    feat : 이진 변환 반복 풀이

---
+++ Ex0805.java
@@ -0,0 +1,33 @@
+public class Ex0805 {
+    
+    public static void main(String[] args) {
+        
+    }
+
+    // https://school.programmers.co.kr/learn/courses/30/lessons/70129
+    class Solution {
+        public int[] solution(String s) {
+            int[] answer = new int[2];
+
+            int zero = 0;
+            int count = 0;
+            int l = 0;
+            while(!s.equals("1")){
+                zero++;
+                l = 0;
+                for(int i =0 ; i < s.length(); i++){
+                    if(s.charAt(i) == '0'){
+                        count++;
+                    }else{
+                        l++;
+                    }
+                }
+
+                s = Integer.toBinaryString(l);
+            }
+            answer[0] = zero;
+            answer[1] = count;
+            return answer;
+        }
+    }
+}
