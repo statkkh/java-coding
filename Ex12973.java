171d5eb703fc514072e7896c981ae6b9fac0fec6
kkh <qr21@naver.com>
2024-04-06 11:06:32 +0900

    feat : programmers 짝지어 제거하기 풀이
    stack을 string 대신 Character타입으로 받기

--- Ex12973.java
+++ Ex12973.java
@@ -6,16 +6,16 @@ public class Ex12973 {
         
         String s = "baabaa";
         Hint hint = new Hint();
-                
+
     }
     
 }
+// https://school.programmers.co.kr/learn/courses/30/lessons/12973
 
 class Hint{
     public int solution(String s){
 
-        String[] str = s.split("");
-
+        // string -> 시간 초과 원인이 됨
         Stack<Character> stack = new Stack<>();
 
         for(int i = 0 ; i < s.length() ; i++){
